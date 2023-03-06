4cdb7abc6acf3b59aa72d53b33a590fcc556070c
Ayussh <ayusshkhurana7@gmail.com>
2023-03-07 02:28:42 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
-var msgObj = "Clicked"
-document.getElementById('edit-btn').addEventListener('click', function () {
-    chrome.tabs.query({}, tabs => {
-        tabs.forEach(tab => {
-            chrome.tabs.sendMessage(tab.id, msgObj);
-        })
-    })
-})
+var msgObj = "Clicked";
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('edit-btn').addEventListener('click', function () {
+      chrome.tabs.query({}, tabs => {
+          tabs.forEach(tab => {
+              chrome.tabs.sendMessage(tab.id, msgObj);
+          })
+      })
+  });
+});
